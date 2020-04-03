252999fd45696e01efd91fd50ddd38455cc40530
chengang <172577882@qq.com>
2020-04-03 22:27:02 +0800

    新增leecode 179提

---
+++ src/main/java/com/tigerjoys/cg/algorithm/leetcode/_0179LargestNumber.java
@@ -0,0 +1,50 @@
+package com.tigerjoys.cg.algorithm.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 
+ * 难度：medium
+ * 题目：给定非负整数数组，重组其序使其形成最大值。
+ * 思路：定制排序算法的比较策略，nums[i]拼接nums[j] 与 nums[j]拼接nums[i] 相比。
+ *
+ */
+public class _0179LargestNumber {
+	
+	public static void main(String[] args) {
+		int[] i = new int[] {10,80,2,9};
+		System.err.println(largestNumber(i));
+	}
+	
+	public static String largestNumber(int[] nums) {
+		if(nums == null || nums.length == 0) {
+			return "0";
+		}
+		if(nums.length == 1) {
+			return String.valueOf(nums[0]);
+		}
+		
+		String[] ss = new String[nums.length];
+		for(int i=0;i<ss.length;i++) {
+			ss[i] = String.valueOf(nums[i]);
+		}
+		
+		/*Arrays.sort(ss, (s1,s2)->{
+			String s1s2 = s1 + s2;
+			String s2s1 = s2 + s1;
+			return s2s1.compareTo(s1s2);
+		});*/
+		
+		Arrays.sort(ss, (s1,s2)->{
+			return s2.compareTo(s1);
+		});
+		
+		StringBuilder buf = new StringBuilder();
+		for(int i=0;i<ss.length;i++) {
+			buf.append(ss[i]);
+		}
+		
+		return buf.toString();
+	}
+
+}
